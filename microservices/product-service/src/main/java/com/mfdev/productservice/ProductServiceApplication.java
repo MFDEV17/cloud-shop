e9153d99c1714560134f96c39bd49158cc35795f
Maxim Sushkin <63682133+MFDEV17@users.noreply.github.com>
2023-02-21 05:50:20 +0400

    fixed component scans

--- microservices/product-service/src/main/java/com/mfdev/productservice/ProductServiceApplication.java
+++ microservices/product-service/src/main/java/com/mfdev/productservice/ProductServiceApplication.java
@@ -1,13 +1,20 @@
 package com.mfdev.productservice;
 
+import com.mfdev.productservice.controller.ProductController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
-@ComponentScan("com.mfdev.api")
-@EntityScan("com.mfdev.api.core.entity")
+@ComponentScans({
+        @ComponentScan("com.mfdev.api"),
+        @ComponentScan(basePackageClasses = ProductController.class)
+})
+@EntityScan("com.mfdev")
 public class ProductServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);

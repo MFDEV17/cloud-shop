308757b8af7edda8b83ef00cce04448f95cfb3fa
Maxim Sushkin <63682133+MFDEV17@users.noreply.github.com>
2023-02-21 05:49:26 +0400

    added lombok annotations

--- api/app-api/src/main/java/com/mfdev/api/core/product/dto/ShortProductDto.java
+++ api/app-api/src/main/java/com/mfdev/api/core/product/dto/ShortProductDto.java
@@ -1,7 +1,16 @@
 package com.mfdev.api.core.product.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class ShortProductDto {
     private String name;
     private String description;
